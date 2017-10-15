9adfbb92812369d9ce121f7afa208c82f5cb1962
ericyoung730 <ericyoung730@gmail.com>
2017-10-15 22:06:42 +0800

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,24 @@
+#example3{
+  width:1600px;
+}
+#button1{
+
+  top:380px;
+  left:785px;
+}
+#button2{
+  
+  top:330px;
+  left:785px;
+}
+#button3{
+ 
+  top:330px;
+  left:785px;
+}
+.content{
+  text-align:center;
+}
+.image{
+   left:600px;
+}
